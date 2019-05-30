5faf74c27bc76b14facd70f4e628b80187446656
Marija Janeckaitė <marija.janeckaite@gmail.com>
2019-05-30 14:08:15 +0300

    Initial Commit

---
+++ BullsEye/AboutTheAuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutTheAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Marija Janeckaitė on 23/05/2019.
+//  Copyright © 2019 Marija Janeckaitė. All rights reserved.
+//
+
+import UIKit
+
+class AboutTheAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Marija Janeckaitė on 23/05/2019.
+//  Copyright © 2019 Marija Janeckaitė. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
